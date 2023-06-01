cd319cdb1253191e513ec0a9a665a6e9c9f681d2
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-01 23:49:10 +0300

    My first commit!

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        System.out.print(number > 0);
+        boolean isPositive = Boolean.valueOf(number > 0);
+        System.out.println(isPositive);
     }
 }
