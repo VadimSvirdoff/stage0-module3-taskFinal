b2442021c257934f7b8bd1f11b520bfc0afce7a5
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-01 23:09:36 +0300

    My first commit!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+
+        // Extract each digit and add it to the sum
+        sum += number % 10;        // Extract the last digit
+        sum += (number / 10) % 10; // Extract the third digit
+        sum += (number / 100) % 10;// Extract the second digit
+        sum += number / 1000;      // Extract the first digit
+
+        System.out.println("Sum of digits: " + sum);
     }
 }
